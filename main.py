ace730891b1dcb72016ab169b3f3533fe8e6ceb3
UnsignedArduino <UnsignedArduino@outlook.com>
2022-03-30 21:47:42 -0400

    Can connect and send text to terminal

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 from argparse import ArgumentParser
+
 from serial.tools.list_ports import comports
 
 from logger import create_logger
+from uno_lcd_terminal import UnoLCDTerminal
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
 
@@ -10,6 +12,28 @@ parser = ArgumentParser(description="Daemon for the Uno LCD Terminal.")
 parser.add_argument("-l", "--list-ports", dest="list_ports",
                     action="store_true",
                     help="List serial ports and exit.")
+parser.add_argument("-c", "--connect", dest="connect",
+                    action="store_true",
+                    help="Connect to an Uno LCD Terminal.")
+parser.add_argument("-p", "--port", dest="port", metavar="PORT",
+                    action="store",
+                    help="A port to connect to. Required if connecting with "
+                         "-c or --connect. Can be a path or an index from "
+                         "-l or --list-ports.")
+parser.add_argument("-u", "--update-interval", dest="update_interval",
+                    metavar="INTERVAL", action="store", type=int,
+                    default=1,
+                    help="The update interval for the Uno LCD Terminal in "
+                         "integer seconds. "
+                         "Required if connecting with -c or --connect. "
+                         "Defaults to 1.")
+parser.add_argument("-r", "--change-interval", dest="change_interval",
+                    metavar="INTERVAL", action="store", type=int,
+                    default=5,
+                    help="The change interval for the Uno LCD Terminal in "
+                         "integer seconds. "
+                         "Required if connecting with -c or --connect. "
+                         "Defaults to 1.")
 args = parser.parse_args()
 logger.debug(f"Arguments received: {args}")
 
@@ -35,5 +59,26 @@ if args.list_ports:
             if value is None:
                 continue
             logger.debug(f"{indent_space}{label}: {value}")
+elif args.connect:
+    port_path = args.port
+    if port_path.replace("-", "").isnumeric():
+        port_path = int(port_path)
+        logger.debug(f"Finding port at index {port_path}")
+        ports = sorted(comports(), key=lambda p: p.name)
+        if port_path > len(ports) or port_path < 1:
+            logger.error(f"No port at index {port_path}! "
+                         f"(out of {len(ports)} ports)")
+            exit(1)
+        port_path = ports[port_path - 1].device
+    logger.info(f"Connecting to {port_path}")
+    term = UnoLCDTerminal(port_path)
+    try:
+        term.run(args.update_interval, args.change_interval,
+                 [
+                     lambda: "Function 1",
+                     lambda: "Function 2"
+                 ])
+    except KeyboardInterrupt:
+        logger.warning("Exiting!")
 else:
     logger.warning("Nothing to do!")
---
+++ uno_lcd_terminal.py
@@ -0,0 +1,155 @@
+import logging
+from enum import Enum
+from time import sleep, time as unix
+from typing import Callable, Optional, Sized
+
+from serial import Serial
+
+from logger import create_logger
+
+logger = create_logger(name=__name__, level=logging.DEBUG)
+
+
+class UnoLCDTerminalAttribute(Enum):
+    WIDTH = b"w"
+    HEIGHT = b"h"
+    HAS_COLOR = b"c"
+
+
+class UnoLCDTerminalBacklightColor(Enum):
+    OFF = b"o"
+    RED = b"r"
+    YELLOW = b"y"
+    GREEN = b"g"
+    TEAL = b"t"
+    BLUE = b"b"
+    VIOLET = b"v"
+    WHITE = b"w"
+
+
+class UnoLCDTerminal:
+    """
+    Represents a Uno LCD Terminal.
+    """
+
+    def __init__(self, path: str):
+        """
+        Initiate a Uno LCD Terminal wrapper at a device path.
+
+        :param path: The device path to the terminal. (for example, "COM8"
+         or "/dev/ttyUSB0")
+        """
+        self.path = path
+
+    def run(self, update_interval: int, change_interval: int,
+            output_funcs: list[Callable]):
+        """
+        Connect to the device and start sending text.
+
+        :param update_interval: The update interval, in integer seconds.
+        :param change_interval: The time between switching output functions,
+         in integer seconds.
+        :param output_funcs: A Sized of output functions that we will step
+         through every change interval.
+        """
+        logger.debug(f"Opening path {self.path}")
+        with Serial(self.path) as port:
+            logger.info("Connected!")
+            logger.debug(f"Update interval: {update_interval}s")
+            logger.debug(f"Change interval: {change_interval}s")
+            logger.debug(f"Output functions: {len(output_funcs)}")
+            last_change = unix()
+            curr_func_idx = 0
+            port.write(UnoLCDTerminal.reset_sequence())
+            port.write(UnoLCDTerminal.set_background_color_sequence(
+                UnoLCDTerminalBacklightColor.WHITE))
+            while True:
+                if unix() - last_change > change_interval:
+                    last_change = unix()
+                    curr_func_idx += 1
+                    if curr_func_idx > len(output_funcs) - 1:
+                        curr_func_idx = 0
+                port.write(UnoLCDTerminal.clear_sequence())
+                port.write(UnoLCDTerminal.home_sequence())
+                output = output_funcs[curr_func_idx]()
+                for index, line in enumerate(output.split("\n")):
+                    port.write(UnoLCDTerminal.set_cursor_sequence(row=index))
+                    port.write(UnoLCDTerminal.write_string_sequence(line))
+                sleep(update_interval)
+
+    @staticmethod
+    def reset_sequence() -> bytes:
+        """
+        Return the reset sequence.
+
+        :return: Bytes.
+        """
+        return b"r"
+
+    @staticmethod
+    def clear_sequence() -> bytes:
+        """
+        Return the clear sequence.
+
+        :return: Bytes.
+        """
+        return b"d"
+
+    @staticmethod
+    def home_sequence() -> bytes:
+        """
+        Return the home sequence.
+
+        :return: Bytes.
+        """
+        return b"h"
+
+    @staticmethod
+    def set_cursor_sequence(col: Optional[int] = None,
+                            row: Optional[int] = None) -> bytes:
+        """
+        Return the set cursor sequence.
+
+        :param col: The new column to set to.
+        :param row: The new row to set to.
+        :return: Bytes.
+        """
+        b = b""
+        if col is not None:
+            b += b"cc"
+            b += str(col).encode()
+        if row is not None:
+            b += b"cr"
+            b += str(row).encode()
+        return b
+
+    @staticmethod
+    def get_attribute_sequence(attr: UnoLCDTerminalAttribute) -> bytes:
+        """
+        Return the get attribute sequence.
+
+        :param attr: The attribute to get for.
+        :return: Bytes.
+        """
+        return b"a" + attr.value
+
+    @staticmethod
+    def write_string_sequence(string: str) -> bytes:
+        """
+        Return the write string sequence.
+
+        :param string: The string to write.
+        :return: Bytes.
+        """
+        return b"p" + string.encode() + b"\n"
+
+    @staticmethod
+    def set_background_color_sequence(
+            color: UnoLCDTerminalBacklightColor) -> bytes:
+        """
+        Return the set background color sequence.
+
+        :param color: The color to set to.
+        :return: Bytes.
+        """
+        return b"b" + color.value
