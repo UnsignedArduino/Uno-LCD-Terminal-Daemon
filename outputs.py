35684725ec04aac36fca1bd64956616c695dfadf
UnsignedArduino <UnsignedArduino@outlook.com>
2022-03-31 18:51:03 -0400

    Add CPU output functions

--- outputs.py
+++ outputs.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import psutil
+
 
 def time_function() -> tuple[str, ...]:
     """Shows the date and time."""
@@ -9,6 +11,30 @@ def time_function() -> tuple[str, ...]:
            now.strftime("%H:%M:%S")
 
 
+def cpu_function() -> tuple[str, ...]:
+    """Shows the CPU utilization"""
+    percent = psutil.cpu_percent()
+    times = psutil.cpu_times()
+    all_times = (
+        f"U: {times.user:.3f}",
+        f"S: {times.system:.3f}",
+        f"I: {times.idle:.3f}"
+    )
+    try:
+        cpu_function.curr_stat_time -= 1
+        if cpu_function.curr_stat_time == 0:
+            cpu_function.curr_stat_time = 5
+            cpu_function.stat_cycle += 1
+            if cpu_function.stat_cycle > len(all_times) - 1:
+                cpu_function.stat_cycle = 0
+    except AttributeError:
+        cpu_function.curr_stat_time = 5
+        cpu_function.stat_cycle = 0
+    return f"CPU: {percent}%", \
+           all_times[cpu_function.stat_cycle]
+
+
 OUTPUT_FUNCTIONS = {
-    "time": time_function
+    "time": time_function,
+    "cpu": cpu_function
 }
